b517e4e1ace0f0ede017b6dba6019323199f0246
Juan A Cortes <juan.cortes@gmail.com>
2016-10-15 18:45:43 -0400

    System stores historical versions of the page each time it is changed, and the user can view the historical versions.

--- server.py
+++ server.py
@@ -13,23 +13,40 @@ def home_page():
 
 @app.route("/<page_name>")
 def place_holder(page_name):
+    # Query database looking for existing information for the page called by the user
     query = db.query("select * from page where title = '%s'" % page_name).namedresult()
+    # No information was found in the database for the page
     if len(query) == 0:
         return render_template(
             "placeholderpage.html",
             page_name = page_name,
             query=query
         )
+    # Information was found in the database for the page
     else:
-        page_content = query[0].page_content
+        query = query[0]
+        print query
+        # Query database looking for historical information for the page called by the user
+        query_history = db.query("select * from page_history where page_id = '%s' order by version_number DESC;" % query.id).namedresult()
+        print query_history
+        page_content = query.page_content
         wiki_linkified_content = wiki_linkify(page_content)
-        print page_content
-        return render_template(
-            "placeholderpage.html",
-            page_name = page_name,
-            query=query[0],
-            page_content = Markup(markdown.markdown(wiki_linkified_content))
-        )
+
+        if len(query_history) > 0:
+            return render_template(
+                "placeholderpage.html",
+                page_name = page_name,
+                query = query,
+                page_content = Markup(markdown.markdown(wiki_linkified_content)),
+                query_history = query_history
+            )
+        else:
+            return render_template(
+                "placeholderpage.html",
+                page_name = page_name,
+                query = query,
+                page_content = Markup(markdown.markdown(wiki_linkified_content)),
+            )
 
 @app.route("/<page_name>/edit")
 def edit_page(page_name):
@@ -49,29 +66,49 @@ def edit_page(page_name):
 
 @app.route("/<page_name>/save", methods=["POST"])
 def save_content(page_name):
+    action = request.form.get("submit_button")
+
+    if action == "update":
+        query = db.query("select * from page where title = '%s'" % page_name)
+        result_list = query.namedresult()
+        print result_list
+        result_list = result_list[0]
+        print result_list
+        db.insert(
+            "page_history",
+            title = page_name,
+            page_content = result_list.page_content,
+            author_name = result_list.author_name,
+            last_mod_date = result_list.last_mod_date,
+            page_id = result_list.id,
+            version_number = result_list.version_number
+        )
+
     current_time = datetime.now()
     last_mod_time = current_time.strftime('%Y/%m/%d %H:%M:%S')
-    print "Last mod time: '%s'" % last_mod_time
     id = request.form.get("id")
     page_content = request.form.get("page_content")
     author_name = request.form.get("author_name")
     last_mod_date = request.form.get("last_mod_date")
-    action = request.form.get("submit_button")
+    version_number = request.form.get("version_number")
     if action == "update":
         db.update(
             "page", {
                 "id": id,
                 "page_content": page_content,
                 "author_name": author_name,
-                "last_mod_date": last_mod_time
+                "last_mod_date": last_mod_time,
+                "version_number": int(version_number) + 1
             }
         )
     elif action == "create":
         db.insert (
             "page",
-            title=page_name,
-            page_content=page_content,
-            author_name=author_name
+            title = page_name,
+            page_content = page_content,
+            author_name = author_name,
+            last_mod_date = last_mod_time,
+            version_number = 1
         )
     else:
         pass
